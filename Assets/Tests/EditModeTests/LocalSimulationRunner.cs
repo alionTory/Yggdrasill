2ab68122cb1f2cffc4ca6d31bd0203b427c31ddc
오건호 <58841595+alionTory@users.noreply.github.com>
2026-07-07 19:15:56 +0900

    테스트 코드 임시 저장

---
+++ Assets/Tests/EditModeTests/LocalSimulationRunner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Photon.Deterministic;
+using Quantum;
+
+namespace Yggdrasill.Tests.EditMode
+{
+    public class LocalSimulationRunner
+    {
+        /// <summary>
+        /// 시뮬레이션 시작
+        /// </summary>
+        public LocalSimulationRunner()
+        {
+
+        }
+
+        /// <summary>
+        /// 시뮬레이션 종료.
+        /// </summary>
+        public void ShutDown()
+        {
+            
+        }
+
+        /// <summary>
+        /// 로컬 플레이어 추가
+        /// </summary>
+        public void AddPlayer(Int32 playerIndex)
+        {
+        }
+        
+        /// <summary>
+        /// <see cref="playerIndex"/>에 해당하는 플레이어가 <see cref="command"/>를 보내도록 함.
+        /// </summary>
+        public void SendCommand(DeterministicCommand command, Int32 playerIndex)
+        {
+        }
+
+        public Frame VerifiedFrame;
+
+
+    }
+}
--- Assets/Tests/EditModeTests/SpawnSeedlingCommandTests.cs
+++ Assets/Tests/EditModeTests/SpawnSeedlingCommandTests.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using NUnit.Framework;
 using Photon.Deterministic;
 using Quantum;
