8702432014f4a58a4a2ec89b586d3a9e53e87e92
오건호 <58841595+alionTory@users.noreply.github.com>
2026-07-26 02:57:42 +0900

    1. StreamJsonRpc 명세는 반환 타입이 Awaitable이 아니라 Task여야 하므로 수정. 2. WaitUNtilSceneLoad API 추가

--- Assets/Tests/E2eTests/TestHookApi.cs
+++ Assets/Tests/E2eTests/TestHookApi.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using QuantumUser.View;
 using UnityEngine;
 using Tests.E2eTests.ClickPointProvider;
 
@@ -6,17 +9,28 @@ namespace Tests.E2eTests
 {
     public class TestHookApi
     {
-        public async Awaitable ClickObject(GameObjectId gameObjectId)
+        public virtual async Task ClickObject(GameObjectId gameObjectId)
         {
             var gameObject = GameObjectRegistryForTest.Get(gameObjectId);
             var clickPointProvider = gameObject.GetComponent<IClickPointProvider>();
-            if(clickPointProvider == null)
+            if (clickPointProvider == null)
                 throw new Exception($"{gameObjectId}에 IClickPointProvider가 없음.");
-            
+
             var clickPoint = clickPointProvider.GetScreenPoint();
             await VirtualDevice.ClickAt(clickPoint);
         }
 
-        
+        public virtual async Task WaitUntilSceneLoad(SceneInfo sceneInfo, CancellationToken cancellationToken)
+        {
+            bool sceneLoaded = false;
+            while (!sceneLoaded)
+            {
+                await Awaitable.NextFrameAsync(cancellationToken);
+                if (sceneInfo.scene.TryGetLoadedScene(out var loadedScene))
+                {
+                    if (loadedScene.IsValid()) sceneLoaded = true;
+                }
+            }
+        }
     }
 }
