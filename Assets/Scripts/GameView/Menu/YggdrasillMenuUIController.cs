05b39f9b9774c9b0c37872bd9b61f0833ec35206
오건호 <58841595+alionTory@users.noreply.github.com>
2026-08-21 19:18:30 +0900

    YggdrasillMenuUIController 작성

---
+++ Assets/Scripts/GameView/Menu/YggdrasillMenuUIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Quantum.Menu;
+
+namespace QuantumUser.View.Menu
+{
+    public class YggdrasillMenuUIController : QuantumMenuUIController
+    {
+        public override async Task HandleConnectionResult(ConnectResult result, QuantumMenuUIController controller)
+        {
+            if (result.CustomResultHandling) return;
+
+            if (result.Success)
+            {
+                Show<YggdrasillUIGamePlay>();
+            }
+            else if (result.FailReason != ConnectFailReason.ApplicationQuit)
+            {
+                var popup = PopupAsync(result.DebugMessage, "접속 실패");
+                if (result.WaitForCleanup != null) await Task.WhenAll(result.WaitForCleanup, popup);
+                else
+                    await popup;
+                Show<YggdrasillUIMain>(); // ← 요구사항: OK 누르면 초기 화면
+            }
+        }
+    }
+}
