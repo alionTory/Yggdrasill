ea69cca1f08e979d816f14360b5528819da77602
오건호 <58841595+alionTory@users.noreply.github.com>
2026-08-21 16:50:14 +0900

    안 쓰는 using 문 제거

--- Assets/Scripts/GameView/Menu/YggdrasillMenuConnection.cs
+++ Assets/Scripts/GameView/Menu/YggdrasillMenuConnection.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
-using NUnit.Framework;
 using Photon.Deterministic;
 using Photon.Realtime;
 using Quantum;
