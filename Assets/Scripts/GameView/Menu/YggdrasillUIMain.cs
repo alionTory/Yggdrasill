207f9bfd7aa6d95d67d43bc609b7291a21c3b205
오건호 <58841595+alionTory@users.noreply.github.com>
2026-08-21 18:54:26 +0900

    메인, 멀티플레이 GUI 담당 클래스 작성

---
+++ Assets/Scripts/GameView/Menu/YggdrasillUIMain.cs
@@ -0,0 +1,41 @@
+﻿using Quantum.Menu;
+using UnityEngine;
+
+namespace QuantumUser.View.Menu
+{
+    public class YggdrasillUIMain : QuantumMenuUIScreen
+    {
+        
+        /// <summary>
+        /// 비-소유 연결 관리 객체
+        /// </summary>
+        public YggdrasillMenuConnection? ConnectionManager { get; set; }
+
+        public override void Awake()
+        {
+            base.Awake();
+            // 게임 창 포커스가 떠나도 게임이 계속 실행되도록 함.
+            if (!Application.runInBackground) Application.runInBackground = true;
+        }
+
+        public override void Init()
+        {
+            base.Init();
+            ConnectionArgs.SetDefaults(Config);
+        }
+
+        public virtual async Awaitable OnSinglePlayButtonPressed()
+        {
+            Contract.RequireNotNull(ConnectionManager);
+            
+            Controller.Show<QuantumMenuUILoading>();
+            var connectionResult = await ConnectionManager.StartLocalAsync(ConnectionArgs);
+            await Controller.HandleConnectionResult(connectionResult, Controller);
+        }
+        
+        public virtual void OnMultiplayButtonPressed()
+        {
+            Controller.Show<YggdrasillUIMultiplay>();
+        }
+    }
+}
---
+++ Assets/Scripts/GameView/Menu/YggdrasillUIMultiplay.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using Quantum.Menu;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace QuantumUser.View.Menu
+{
+    public class YggdrasillUIMultiplay : QuantumMenuUIScreen
+    {
+        [SerializeField] private InputField invitationCodeField = null!;
+
+        /// <summary>
+        /// 비-소유 연결 관리 객체
+        /// </summary>
+        public YggdrasillMenuConnection? ConnectionManager { get; set; }
+
+        public virtual async Task OnAutoMatchingButtonPressed()
+        {
+            Controller.Show<QuantumMenuUILoading>();
+
+            ConnectionArgs.Session = null;
+            ConnectionArgs.Creating = true;
+            var result = await ConnectionManager.StartOnlineAsync(ConnectionArgs);
+
+            await Controller.HandleConnectionResult(result, Controller);
+        }
+
+        public virtual async Task OnPrivateRoomCreateButtonPressed()
+        {
+            var code = Config.CodeGenerator.Create();
+            Controller.Show<QuantumMenuUILoading>();
+            Controller.Get<QuantumMenuUILoading>().SetStatusText($"참가 코드: {code}");
+
+            ConnectionArgs.Session = code;
+            ConnectionArgs.Creating = true;
+            var result = await ConnectionManager.StartOnlineAsync(ConnectionArgs);
+
+            await Controller.HandleConnectionResult(result, Controller);
+        }
+
+        public virtual async Task OnPrivateRoomParticipateButtonPressed()
+        {
+            var code = invitationCodeField.text.ToUpperInvariant();
+            if (!Config.CodeGenerator.IsValid(code))
+            {
+                await Controller.PopupAsync("참가 코드 형식이 올바르지 않습니다.", "참가 실패");
+            }
+            else
+            {
+                Controller.Show<QuantumMenuUILoading>();
+
+                ConnectionArgs.Session = code;
+                ConnectionArgs.Creating = false;
+                var result = await ConnectionManager.StartOnlineAsync(ConnectionArgs);
+
+                await Controller.HandleConnectionResult(result, Controller);
+            }
+        }
+    }
+}
