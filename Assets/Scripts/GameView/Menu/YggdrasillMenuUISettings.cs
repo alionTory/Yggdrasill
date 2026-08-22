948031920db6994fcb4e63945a2fefa66abfaeb9
오건호 <58841595+alionTory@users.noreply.github.com>
2026-08-23 01:25:35 +0900

    Settings 메뉴 스크린 프리팹에서 커스텀 메인 메뉴를 호출하도록 수정

---
+++ Assets/Scripts/GameView/Menu/YggdrasillMenuUISettings.cs
@@ -0,0 +1,12 @@
+﻿using Quantum.Menu;
+
+namespace QuantumUser.View.Menu
+{
+    public class YggdrasillMenuUISettings : QuantumMenuUISettings
+    {
+        public override void OnBackButtonPressed()
+        {
+            Controller.Show<YggdrasillUIMain>();
+        }
+    }
+}
